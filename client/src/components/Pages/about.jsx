8f2b2b05ca01947e6feb6de1a3a350e237f623df
kadrikondi <ajigosokowo455@gmail.com>
2019-11-21 15:10:23 +0100

    farm better prescriptive system

--- client/src/components/Pages/about.jsx
+++ client/src/components/Pages/about.jsx
@@ -4,13 +4,13 @@ export default class About extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <div className="col-lg-8 card">
-            <h2>
+        <div className="row mt-5">
+          <div className="col-lg-8 card mt-5">
+            <h2 className="p-5 text-left">
               This is farm prescriptive system that recommeds action to farmers
               in order to get high crop Yield{" "}
             </h2>
-            <h3>
+            <h3 className="text-center p-5">
               Design by Idris Abdulkadri as Final Year Project in Computer
               Science Department Fulokoja
             </h3>
