33860c336f7ab6381aaec28f10278548d1c629f0
kadrikondi <ajigosokowo455@gmail.com>
2019-11-21 14:56:44 +0100

    farm better prescriptive system

--- client/src/components/Pages/about.jsx
+++ client/src/components/Pages/about.jsx
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class About extends Component {
-    render() {
-        return (
-            <div>
-               <h2>This is farm prescriptive system that recommeds action to farmers  in order to get high crop Yield </h2> 
-               <h3>Design by Idris Abdulkadri as Final Year Project in Computer Science Department Fulokoja</h3>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 card">
+            <h2>
+              This is farm prescriptive system that recommeds action to farmers
+              in order to get high crop Yield{" "}
+            </h2>
+            <h3>
+              Design by Idris Abdulkadri as Final Year Project in Computer
+              Science Department Fulokoja
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- client/src/components/Pages/contact.jsx
+++ client/src/components/Pages/contact.jsx
@@ -3,20 +3,25 @@ import React, { Component } from "react";
 export default class Contact extends Component {
   render() {
     return (
-      <div>
-        <h2>Contact Information</h2>
-        <ul>
-          <li className="list-group-item">
-            <b>Name:Idris Abdulkadri</b>
-          </li>
-          <li className="list-group-item">
-            <b>Email:Abdulkadri42@gmail.com</b>
-          </li>
-          <a href="https://www.twitter.com/kadrikondi">
-            {" "}
-            Connect me on Twitter
-          </a>
-        </ul>
+      <div className="container">
+        <div className="row ">
+          <div className="col-lg-4"></div>
+          <div className="col-lg-6 card">
+            <h2>Contact Information</h2>
+            <ul>
+              <li className="list-group-item">
+                <b>Name:Idris Abdulkadri</b>
+              </li>
+              <li className="list-group-item">
+                <b>Email:Abdulkadri42@gmail.com</b>
+              </li>
+              <a href="https://www.twitter.com/kadrikondi">
+                {" "}
+                Connect me on Twitter
+              </a>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
