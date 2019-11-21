be3f99c71b9c94f43a89de29e74e01cea50fefb6
kadrikondi <abdulkadri42@gmail.com>
2019-11-21 18:14:47 +0100

    prescritive system heroku

--- client/src/components/Pages/LandingPage/Particlejs.jsx
+++ client/src/components/Pages/LandingPage/Particlejs.jsx
@@ -1,63 +1,57 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Particles from 'react-particles-js';
+import Particles from "react-particles-js";
 
-import './Particlejs.css'
+import "./Particlejs.css";
 
+const particleOption = {
+  particles: {
+    number: {
+      value: 80,
+      color: "#000000",
 
-const particleOption={
-    particles: {
-    number:{
-      value:80,
-      color:"#000000",
-  
-      density:{
-        enable:true,
-        value_area:800
-      },
-
+      density: {
+        enable: true,
+        value_area: 800
+      }
     }
-   
-    
   }
-  
-  
-      
-  }  
+};
 
-
-const Particlejs=()=>{
-
- 
-return(
-
+const Particlejs = () => {
+  return (
     <div>
-       <section >
-        <Particles  className="particle"
-              params={{particleOption}}/>
-     
-       <div className="pSection">
-      <h1 className="mb-5">Prescriptive System for crop yield</h1>
-        
-      <br/>
-      <div style={{margin:'10px', opacity:'1!important'}}>
-     
-          <Link to="/signup"> <button className="btn btn-white">Register</button></Link>
+      <section>
+        <Particles className="particle" params={{ particleOption }} />
 
-          <Link to="/signin"> <button className="btn btn-white">Signin</button></Link>
+        <div className="pSection">
+          <h1 className="mb-5">Prescriptive System for crop yield</h1>
 
+          <br />
+          <div style={{ margin: "10px", opacity: "1!important" }}>
+            <Link to="/signup">
+              {" "}
+              <button className="btn btn-white">Register</button>
+            </Link>
+
+            <Link to="/signin">
+              {" "}
+              <button className="btn btn-white">Signin</button>
+            </Link>
           </div>
 
-            <br/>
-            <h3>Predict, recommends & Get expert advice on crop yield </h3> 
-          </div>
-          </section>
-
+          <br />
+          <h3>Predict, recommends & Get expert advice on crop yield </h3>
+          <p className="text-left">
+            DEVELOPE BY IDRIS ABDULKADRI IN PARTIAL FULFILLMENT OF THE
+            REQUIREMENTS FOR THE AWARD OF THE BACHELOR OF SCIENCE (HONOURS)
+            DEGREE IN COMPUTER SCIENCE
+          </p>
+        </div>
+      </section>
     </div>
-)
+  );
+};
 
-}
-
-
-export default Particlejs
+export default Particlejs;
